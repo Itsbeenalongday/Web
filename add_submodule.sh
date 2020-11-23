c2fabfe3740f3a33957d0a9d46470bc1817bc034
Itsbeeanlongday <dbtjdals1771@ajou.ac.kr>
2020-11-23 21:29:28 +0900

    update agileboard

--- add_submodule.sh
+++ add_submodule.sh
@@ -11,23 +11,23 @@ echo "#                                          #"
 echo "#                                          #"
 echo "#                                          #"
 echo "############################################"
-echo -e "\n"
+echo "\n"
 while true; do
     read -p "Do you want rename repository? [y/n] >> " option
-    if [ $option = "y" ] || [ $option = "n" ]; then
+    if ( $option = "y" ] || [ $option = "n" ); then
         break
     else
         echo "Invaild input please enter y or n"
         continue
     fi 
 done
-if [ $option == "y" ]; then
+if ( $option == "y" ); then
     echo -n "enter name for renaming >> "
     read name
     echo -n "enter url for add submodule >> "
     read url
     git submodule add "$url" "$name"
-elif [ $option == "n" ]; then
+elif ( $option == "n" ); then
     echo -n "enter url for add submodule >> "
     read url
     git submodule add "$url"
